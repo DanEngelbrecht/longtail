8b8938d4531e8e528d9687d13ab92ce95ecac809
Dan Engelbrecht <dan@engelbrecht.com>
2019-07-15 17:31:31 +0200

    Added CXXFLAGS_DEBUG

--- build/d_cl.bat
+++ build/d_cl.bat
@@ -1,11 +1,13 @@
 @echo off
 
 set OPT=
-set CXXFLAGS=%CXXFLAGS% /DBIKESHED_ASSERTS
 set OUTPUT=test_debug
 
 call build_src.bat
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 call build\mvsc_build.bat
+
+set CXXFLAGS=%CXXFLAGS% %CXXFLAGS_DEBUG%
+
 exit /B %ERRORLEVEL%
--- build/d_clang.sh
+++ build/d_clang.sh
@@ -5,10 +5,12 @@ set -e
 export OPT="-g"
 ASAN="-fsanitize=address -fno-omit-frame-pointer"
 #CXXFLAGS="$CXXFLAGS -Wall -Weverything -pedantic -Wno-zero-as-null-pointer-constant -Wno-old-style-cast -Wno-global-constructors -Wno-padded"
-export CXXFLAGS="$CXXFLAGS -Wno-deprecated-register -Wno-deprecated -DBIKESHED_ASSERTS"
+export CXXFLAGS="$CXXFLAGS -Wno-deprecated-register -Wno-deprecated"
 export ARCH="-m64 -maes"
 export OUTPUT=test_debug
 
 . ./build_src.sh
+
+export CXXFLAGS="$CXXFLAGS $CXXFLAGS_DEBUG"
 
 sh build/clang_build.sh
