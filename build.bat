eb15993be0aaf0b712101d02835e1bb2f4828838
Dan Engelbrecht <dan@engelbrecht.com>
2019-07-16 23:51:21 +0200

    static lib for third party dependencies

--- build.bat
+++ build.bat
@@ -1,12 +1,19 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
-set RELEASE="%1%"
+if "%1%" = "build-third-party" (
+    set BUILD_THIRD_PARTY=%1%
+    set RELEASE_MODE=%2%
+) else (
+    set BUILD_THIRD_PARTY=
+    set RELEASE_MODE=%1%
+)
 
 if !RELEASE! == "release" (
     set OPT=/O2
     set CXXFLAGS=/nologo /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W3 /wd5045 /wd4514 /wd4710 /wd4820 /wd4820 /wd4668 /wd4464 /wd5039 /wd4255 /wd4626
     set OUTPUT=test
+    set THIRD_PARTY_LIB=third-party.lib
 
     call build_options.bat
     if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
@@ -15,6 +22,7 @@ if !RELEASE! == "release" (
     set OPT=
     set CXXFLAGS=/nologo /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W3 /wd5045 /wd4514 /wd4710 /wd4820 /wd4820 /wd4668 /wd4464 /wd5039 /wd4255 /wd4626
     set OUTPUT=test_debug
+    set THIRD_PARTY_LIB=third-party-debug.lib
 
     call build_options.bat
     if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
@@ -59,8 +67,16 @@ if NOT DEFINED VCINSTALLDIR (
     exit 1
 )
 
+if NOT EXIST build/!THIRD_PARTY_LIB! (
+    set BUILD_THIRD_PARTY=build-third-party
+)
+
 IF NOT EXIST build (
     mkdir build
+)
+
+if !BUILD_THIRD_PARTY! == "build-third-party" (
+    echo "Compiling third party dependencies to library" %THIRD_PARTY_LIB%
 )
 
 cl.exe %CXXFLAGS% %OPT% %SRC% %TEST_SRC% %THIRDPARTY_SRC% /Fd:build\test_vc.pdb /link /out:build\%OUTPUT%.exe /pdb:build\%OUTPUT%.pdb
