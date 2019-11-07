2c511cf2e26eaa7f5f67771304262d649a2d57c2
Dan Engelbrecht <dan@engelbrecht.com>
2019-11-07 09:52:14 +0100

    Fixed list of versions in bench test

--- test/test.cpp
+++ test/test.cpp
@@ -2955,11 +2955,11 @@ void Bench()
 
     const uint32_t VERSION_COUNT = 6;
     const char* VERSION[VERSION_COUNT] = {
-        "git2f7f84a05fc290c717c8b5c0e59f8121481151e6_Win64_Editor"
-        "git916600e1ecb9da13f75835cd1b2d2e6a67f1a92d_Win64_Editor"
-        "gitfdeb1390885c2f426700ca653433730d1ca78dab_Win64_Editor"
-        "git81cccf054b23a0b5a941612ef0a2a836b6e02fd6_Win64_Editor"
-        "git558af6b2a10d9ab5a267b219af4f795a17cc032f_Win64_Editor"
+        "git2f7f84a05fc290c717c8b5c0e59f8121481151e6_Win64_Editor",
+        "git916600e1ecb9da13f75835cd1b2d2e6a67f1a92d_Win64_Editor",
+        "gitfdeb1390885c2f426700ca653433730d1ca78dab_Win64_Editor",
+        "git81cccf054b23a0b5a941612ef0a2a836b6e02fd6_Win64_Editor",
+        "git558af6b2a10d9ab5a267b219af4f795a17cc032f_Win64_Editor",
         "gitc2ae7edeab85d5b8b21c8c3a29c9361c9f957f0c_Win64_Editor"
     };
 #else
