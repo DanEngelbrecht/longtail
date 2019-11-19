2c9869dd4be9ce76ce1020d593239e7fbea5ad9d
Dan Engelbrecht <dan.engelbrecht@embark-studios.com>
2019-11-19 14:47:28 +0100

    Updated to latest trove

--- cmd/do_version.sh
+++ cmd/do_version.sh
@@ -12,7 +12,7 @@ echo Creating chunks for unknown content of version $2/local/$3
 $1 --create-content "$2/$3_chunks" --content-index "$2/$3.lci" --version "$2/local/$3" --version-index "$2/$3.lvi"
 
 echo Adding the new chunks from $2/$3_chunks to $2/chunks/
-if [ -d "$2/chunks" ] mkdir -p "$2/chunks"
+[ -d "$2/chunks" ] || mkdir -p "$2/chunks"
 cp $2/$3_chunks/* $2/chunks
 
 echo Merging the new chunks from $2/local/$3.lci into $2/chunks.lci
