634fbf9e5bc1cceb9abc23de64a8e997ab35b2f0
Dan Engelbrecht <dan@engelbrecht.com>
2019-11-22 01:43:05 +0100

    Fixed build options

--- cmd/build_options.bat
+++ cmd/build_options.bat
@@ -3,4 +3,4 @@ set SRC=..\src\longtail.c main.cpp
 set TEST_SRC=
 set THIRDPARTY_SRC=nadir\src\nadir.cpp lizard\lib\*.c lizard\lib\entropy\*.c lizard\lib\xxhash\*.c trove\src\trove.cpp
 set CXXFLAGS=%CXXFLAGS% /wd4244 /wd4316 /wd4996 /DLONGTAIL_VERBOSE_LOGS
-set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG% /DBIKESHED_ASSERTS /DLONGTAIL_VERBOSE_LOGS /D_DEBUG
+set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG% /DBIKESHED_ASSERTS /DLONGTAIL_VERBOSE_LOGS /D_DEBUG /MDd
