8a4dc16f45a5994988a7512ee454260766415c59
Dan Engelbrecht <dan@engelbrecht.com>
2020-01-11 23:53:19 +0100

    Removed double reference to src/ext

--- test/build_options.bat
+++ test/build_options.bat
@@ -1,6 +1,6 @@
 set TARGET=test
 set SRC=%BASE_DIR%\src\*.c %BASE_DIR%lib\*.c %BASE_DIR%lib\bikeshed\*.c %BASE_DIR%lib\blake2\*.c %BASE_DIR%lib\filestorage\*.c %BASE_DIR%lib\lizard\*.c %BASE_DIR%lib\brotli\*.c %BASE_DIR%lib\memstorage\*.c %BASE_DIR%lib\meowhash\*.c
 set TEST_SRC=%BASE_DIR%\test\test.cpp %BASE_DIR%\test\main.cpp
-set THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c %BASE_DIR%\src\ext\*.c %BASE_DIR%lib\blake2\ext\*.c %BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c %BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
+set THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c %BASE_DIR%lib\blake2\ext\*.c %BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c %BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
 set CXXFLAGS=%CXXFLAGS% /wd4244 /wd4316 /wd4996 /DLONGTAIL_LOG_LEVEL=5 /D__SSE2__
 set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG% /DBIKESHED_ASSERTS /DLONGTAIL_ASSERTS /D_DEBUG /DLONGTAIL_LOG_LEVEL=3 /MDd /DLONGTAIL_VERBOSE_LOGS /D__SSE2__
--- test/build_options.sh
+++ test/build_options.sh
@@ -3,6 +3,6 @@
 export TARGET=test
 export SRC="$BASE_DIR/src/*.c $BASE_DIR/lib/*.c $BASE_DIR/lib/bikeshed/*.c $BASE_DIR/lib/blake2/*.c $BASE_DIR/lib/filestorage/*.c $BASE_DIR/lib/lizard/*.c $BASE_DIR/lib/brotli/*.c $BASE_DIR/lib/memstorage/*.c $BASE_DIR/lib/meowhash/*.c"
 export TEST_SRC="$BASE_DIR/test/main.cpp $BASE_DIR/test/test.cpp"
-export THIRDPARTY_SRC="$BASE_DIR/src/ext/*.c $BASE_DIR/src/ext/*.c $BASE_DIR/lib/blake2/ext/*.c $BASE_DIR/lib/lizard/ext/*.c $BASE_DIR/lib/lizard/ext/entropy/*.c $BASE_DIR/lib/lizard/ext/xxhash/*.c $BASE_DIR/lib/brotli/ext/common/*.c $BASE_DIR/lib/brotli/ext/dec/*.c $BASE_DIR/lib/brotli/ext/enc/*.c $BASE_DIR/lib/brotli/ext/fuzz/*.c"
+export THIRDPARTY_SRC="$BASE_DIR/src/ext/*.c $BASE_DIR/lib/blake2/ext/*.c $BASE_DIR/lib/lizard/ext/*.c $BASE_DIR/lib/lizard/ext/entropy/*.c $BASE_DIR/lib/lizard/ext/xxhash/*.c $BASE_DIR/lib/brotli/ext/common/*.c $BASE_DIR/lib/brotli/ext/dec/*.c $BASE_DIR/lib/brotli/ext/enc/*.c $BASE_DIR/lib/brotli/ext/fuzz/*.c"
 export CXXFLAGS="$CXXFLAGS -pthread -U_WIN32 -DLONGTAIL_LOG_LEVEL=5"
 export CXXFLAGS_DEBUG="$CXXFLAGS_DEBUG -DBIKESHED_ASSERTS -DLONGTAIL_LOG_LEVEL=3 -DLONGTAIL_ASSERTS"
