9dd7548cd46f944ea02e6a521927be4f4bd6d502
Dan Engelbrecht <dan@engelbrecht.com>
2020-01-12 17:36:51 +0100

    Fix/cleanup build scripts (#12)
    * Cleaned up build option files
    
    * Fix missing quotes

--- cmd/build_options.bat
+++ cmd/build_options.bat
@@ -1,7 +1,28 @@
 set TARGET=longtail
-set SRC=%BASE_DIR%src\*.c %BASE_DIR%cmd\main.cpp %BASE_DIR%lib\*.c %BASE_DIR%lib\bikeshed\*.c %BASE_DIR%lib\blake2\*.c %BASE_DIR%lib\filestorage\*.c %BASE_DIR%lib\lizard\*.c %BASE_DIR%lib\memstorage\*.c %BASE_DIR%lib\meowhash\*.c
+
+set LIB_SRC=%BASE_DIR%lib\filestorage\*.c
+set LIB_THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c
+
+set FILESTORAGE_SRC=%BASE_DIR%lib\*.c
+
+set MEMSTORAGE_SRC=%BASE_DIR%lib\memstorage\*.c
+
+set MEOWHASH_SRC=%BASE_DIR%lib\meowhash\*.c
+
+set BIKESHED_SRC=%BASE_DIR%lib\bikeshed\*.c
+
+set BLAKE2_SRC=%BASE_DIR%lib\blake2\*.c
+set BLAKE2_THIRDPARTY_SRC=%BASE_DIR%lib\blake2\ext\*.c
+
+set LIZARD_SRC=%BASE_DIR%lib\lizard\*.c
+set LIZARD_THIRDPARTY_SRC=%BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c
+
+set BROTLI_SRC=%BASE_DIR%lib\brotli\*.c 
+set BROTLI_THIRDPARTY_SRC=%BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
+
+set SRC=%BASE_DIR%cmd\main.cpp %BASE_DIR%\src\*.c %LIB_SRC% %FILESTORAGE_SRC% %MEMSTORAGE_SRC% %MEOWHASH_SRC% %BIKESHED_SRC% %BLAKE2_SRC% %LIZARD_SRC% %BROTLI_SRC%
 set TEST_SRC=
-set THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c %BASE_DIR%lib\blake2\ext\*.c %BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c %BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
+set THIRDPARTY_SRC=%LIB_THIRDPARTY_SRC% %BLAKE2_THIRDPARTY_SRC% %LIZARD_THIRDPARTY_SRC% %BROTLI_THIRDPARTY_SRC%
 set CXXFLAGS=%CXXFLAGS% /wd4244 /wd4316 /wd4996 /D__SSE2__
 set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG% /DBIKESHED_ASSERTS /DLONGTAIL_ASSERTS /D__SSE2__
 rem /D /D_DEBUG /MDd
--- test/build_options.bat
+++ test/build_options.bat
@@ -1,6 +1,27 @@
 set TARGET=test
-set SRC=%BASE_DIR%\src\*.c %BASE_DIR%lib\*.c %BASE_DIR%lib\bikeshed\*.c %BASE_DIR%lib\blake2\*.c %BASE_DIR%lib\filestorage\*.c %BASE_DIR%lib\lizard\*.c %BASE_DIR%lib\brotli\*.c %BASE_DIR%lib\memstorage\*.c %BASE_DIR%lib\meowhash\*.c
-set TEST_SRC=%BASE_DIR%\test\test.cpp %BASE_DIR%\test\main.cpp
-set THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c %BASE_DIR%lib\blake2\ext\*.c %BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c %BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
+
+set LIB_SRC=%BASE_DIR%lib\filestorage\*.c
+set LIB_THIRDPARTY_SRC=%BASE_DIR%\src\ext\*.c
+
+set FILESTORAGE_SRC=%BASE_DIR%lib\*.c
+
+set MEMSTORAGE_SRC=%BASE_DIR%lib\memstorage\*.c
+
+set MEOWHASH_SRC=%BASE_DIR%lib\meowhash\*.c
+
+set BIKESHED_SRC=%BASE_DIR%lib\bikeshed\*.c
+
+set BLAKE2_SRC=%BASE_DIR%lib\blake2\*.c
+set BLAKE2_THIRDPARTY_SRC=%BASE_DIR%lib\blake2\ext\*.c
+
+set LIZARD_SRC=%BASE_DIR%lib\lizard\*.c
+set LIZARD_THIRDPARTY_SRC=%BASE_DIR%lib\lizard\ext\*.c %BASE_DIR%lib\lizard\ext\entropy\*.c %BASE_DIR%lib\lizard\ext\xxhash\*.c
+
+set BROTLI_SRC=%BASE_DIR%lib\brotli\*.c 
+set BROTLI_THIRDPARTY_SRC=%BASE_DIR%lib\brotli\ext\common\*.c %BASE_DIR%lib\brotli\ext\dec\*.c %BASE_DIR%lib\brotli\ext\enc\*.c %BASE_DIR%lib\brotli\ext\fuzz\*.c
+
+set SRC=%BASE_DIR%\src\*.c %LIB_SRC% %FILESTORAGE_SRC% %MEMSTORAGE_SRC% %MEOWHASH_SRC% %BIKESHED_SRC% %BLAKE2_SRC% %LIZARD_SRC% %BROTLI_SRC%
+set TEST_SRC=%BASE_DIR%\test\main.cpp %BASE_DIR%\test\test.cpp
+set THIRDPARTY_SRC=%LIB_THIRDPARTY_SRC% %BLAKE2_THIRDPARTY_SRC% %LIZARD_THIRDPARTY_SRC% %BROTLI_THIRDPARTY_SRC%
 set CXXFLAGS=%CXXFLAGS% /wd4244 /wd4316 /wd4996 /DLONGTAIL_LOG_LEVEL=5 /D__SSE2__
 set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG% /DBIKESHED_ASSERTS /DLONGTAIL_ASSERTS /D_DEBUG /DLONGTAIL_LOG_LEVEL=3 /MDd /DLONGTAIL_VERBOSE_LOGS /D__SSE2__
