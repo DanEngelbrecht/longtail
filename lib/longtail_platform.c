d9f6320acaded0d9fa6315825ff06b2e6eb4a967
Dan Engelbrecht <dan.engelbrecht@embark-studios.com>
2020-01-16 09:59:08 +0100

    Add resolving of ~ when scanning folders Don't use "rb+" mode when opening files

--- lib/longtail_platform.c
+++ lib/longtail_platform.c
@@ -312,7 +312,7 @@ int Longtail_IsDir(const char* path)
         if (e == ENOENT){
             return 0;
         }
-        LONGTAIL_LOG(LONGTAIL_LOG_LEVEL_WARNING, "Can't determine type of `%s`: %d\n", path, e);
+        LONGTAIL_LOG(LONGTAIL_LOG_LEVEL_WARNING, "Can't determine type of `%s`: %d\n", path, e)
         return 0;
     }
     return (attrs & FILE_ATTRIBUTE_DIRECTORY) ? 1 : 0;
@@ -327,7 +327,7 @@ int Longtail_IsFile(const char* path)
         if (e == ENOENT){
             return 0;
         }
-        LONGTAIL_LOG(LONGTAIL_LOG_LEVEL_WARNING, "Can't determine type of `%s`: %d\n", path, e);
+        LONGTAIL_LOG(LONGTAIL_LOG_LEVEL_WARNING, "Can't determine type of `%s`: %d\n", path, e)
         return 0;
     }
     return (attrs & FILE_ATTRIBUTE_DIRECTORY) == 0 ? 1 : 0;
@@ -1087,14 +1087,9 @@ static int Skip(HLongtail_FSIterator fs_iterator)
         fs_iterator->m_DirEntry = readdir(fs_iterator->m_DirStream);
         if (fs_iterator->m_DirEntry == 0)
         {
-            int e = errno;
-            if (e == 0)
-            {
                 return ENOENT;
             }
-            return e;
         }
-    }
     return 0;
 }
 
@@ -1129,22 +1124,21 @@ int Longtail_StartFind(HLongtail_FSIterator fs_iterator, const char* path)
     fs_iterator->m_DirEntry = readdir(fs_iterator->m_DirStream);
     if (fs_iterator->m_DirEntry == 0)
     {
-        int e = errno;
         printf("readdir(%s): %d", path, e);
         closedir(fs_iterator->m_DirStream);
+        fs_iterator->m_DirStream = 0;
         Longtail_Free(fs_iterator->m_DirPath);
-        if (e == 0)
-        {
+        fs_iterator->m_DirPath = 0;
             return ENOENT;
         }
-        return e;
-    }
     int err = Skip(fs_iterator);
     if (err)
     {
         printf("Skip(%s): %d", path, err);
         closedir(fs_iterator->m_DirStream);
+        fs_iterator->m_DirStream = 0;
         Longtail_Free(fs_iterator->m_DirPath);
+        fs_iterator->m_DirPath = 0;
         return err;
     }
     return 0;
@@ -1155,13 +1149,8 @@ int Longtail_FindNext(HLongtail_FSIterator fs_iterator)
     fs_iterator->m_DirEntry = readdir(fs_iterator->m_DirStream);
     if (fs_iterator->m_DirEntry == 0)
     {
-        int e = errno;
-        if (e == 0)
-        {
             return ENOENT;
         }
-        return e;
-    }
     return Skip(fs_iterator);
 }
 
@@ -1224,10 +1213,11 @@ int Longtail_OpenReadFile(const char* path, HLongtail_OpenFile* out_read_file)
 
 int Longtail_OpenWriteFile(const char* path, uint64_t initial_size, HLongtail_OpenFile* out_write_file)
 {
-    FILE* f = fopen(path, initial_size == 0 ? "wb" : "rb+");
+    FILE* f = fopen(path, "wb");
     if (!f)
     {
-        return errno;
+        int e = errno;
+        return e;
     }
     if  (initial_size > 0)
     {
