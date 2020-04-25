2f5517df4316ca0880834ba110dce4c44aa829a4
Dan Engelbrecht <dan@engelbrecht.com>
2020-04-25 13:20:07 +0200

    Added atomiccancel impl of cancel api

---
+++ lib/atomiccancel/longtail_atomiccancel.c
@@ -0,0 +1,89 @@
+#include "longtail_atomiccancel.h"
+
+#include "../../src/longtail.h"
+#include "../longtail_platform.h"
+#include <errno.h>
+
+struct AtomicCancelAPI
+{
+    struct Longtail_CancelAPI m_CancelAPI;
+};
+
+static int AtomicCancelAPI_CreateToken(struct Longtail_CancelAPI* cancel_api, Longtail_CancelAPI_HCancelToken* out_token)
+{
+    LONGTAIL_VALIDATE_INPUT(cancel_api, return EINVAL )
+    LONGTAIL_VALIDATE_INPUT(out_token, return EINVAL )
+    struct AtomicCancelAPI* api = (struct AtomicCancelAPI*)cancel_api;
+    TLongtail_Atomic32* atomic_counter = (TLongtail_Atomic32*)Longtail_Alloc(sizeof(sizeof(TLongtail_Atomic32)));
+    if (!atomic_counter)
+    {
+        return ENOMEM;
+    }
+    *atomic_counter = 0;
+    *out_token = (Longtail_CancelAPI_HCancelToken)atomic_counter;
+    return 0;
+}
+
+static int AtomicCancelAPI_Cancel(struct Longtail_CancelAPI* cancel_api, Longtail_CancelAPI_HCancelToken token)
+{
+    LONGTAIL_VALIDATE_INPUT(cancel_api, return EINVAL )
+    LONGTAIL_VALIDATE_INPUT(token, return EINVAL )
+    struct AtomicCancelAPI* api = (struct AtomicCancelAPI*)cancel_api;
+    TLongtail_Atomic32* atomic_counter = (TLongtail_Atomic32*)token;
+    if (*atomic_counter == 0)
+    {
+        Longtail_AtomicAdd32(atomic_counter, 1);
+    }
+    return 0;
+}
+static int AtomicCancelAPI_DisposeToken(struct Longtail_CancelAPI* cancel_api, Longtail_CancelAPI_HCancelToken token)
+{
+    LONGTAIL_VALIDATE_INPUT(cancel_api, return EINVAL )
+    LONGTAIL_VALIDATE_INPUT(token, return EINVAL )
+    struct AtomicCancelAPI* api = (struct AtomicCancelAPI*)cancel_api;
+    TLongtail_Atomic32* atomic_counter = (TLongtail_Atomic32*)token;
+    Longtail_Free((void*)atomic_counter);
+    return 0;
+}
+
+static int AtomicCancelAPI_IsCancelled(struct Longtail_CancelAPI* cancel_api, Longtail_CancelAPI_HCancelToken token)
+{
+    LONGTAIL_VALIDATE_INPUT(cancel_api, return EINVAL )
+    LONGTAIL_VALIDATE_INPUT(token, return EINVAL )
+    struct AtomicCancelAPI* api = (struct AtomicCancelAPI*)cancel_api;
+    TLongtail_Atomic32* atomic_counter = (TLongtail_Atomic32*)token;
+    int32_t current_value = *atomic_counter;
+    if (*atomic_counter)
+    {
+        return ECANCELED;
+    }
+    return 0;
+}
+
+static void AtomicCancelAPI_Dispose(struct Longtail_API* cancel_api)
+{
+    LONGTAIL_VALIDATE_INPUT(cancel_api, return)
+    Longtail_Free(cancel_api);
+}
+
+static void AtomicCancelAPI_Init(struct AtomicCancelAPI* api)
+{
+    LONGTAIL_FATAL_ASSERT(api, return)
+    api->m_CancelAPI.m_API.Dispose = AtomicCancelAPI_Dispose;
+    api->m_CancelAPI.m_CreateToken = AtomicCancelAPI_CreateToken;
+    api->m_CancelAPI.m_Cancel = AtomicCancelAPI_Cancel;
+    api->m_CancelAPI.m_IsCancelled = AtomicCancelAPI_IsCancelled;
+    api->m_CancelAPI.m_DisposeToken = AtomicCancelAPI_DisposeToken;
+}
+
+struct Longtail_CancelAPI* Longtail_CreateAtomicCancelAPI()
+{
+    struct AtomicCancelAPI* api = (struct AtomicCancelAPI*)Longtail_Alloc(sizeof(struct AtomicCancelAPI));
+    if (!api)
+    {
+        // TOOD: Log
+        return 0;
+    }
+    AtomicCancelAPI_Init(api);
+    return &api->m_CancelAPI;
+}
