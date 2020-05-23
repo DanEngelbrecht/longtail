5f956a4416acf8d422f18cb9f1cf082bf30a90af
Dan Engelbrecht <dan@engelbrecht.com>
2020-05-23 19:38:27 +0200

    Add more search path for visual studio

--- build.bat
+++ build.bat
@@ -68,8 +68,32 @@ if "!RELEASE_MODE!" == "release" (
 )
 
 if NOT DEFINED VCINSTALLDIR (
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\\VC\Auxiliary\Build\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
     )
 )
 
@@ -88,12 +112,6 @@ if NOT DEFINED VCINSTALLDIR (
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-    )
-)
-
-if NOT DEFINED VCINSTALLDIR (
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" amd64
     )
 )
 
