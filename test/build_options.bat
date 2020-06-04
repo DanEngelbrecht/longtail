0962c6397a5c1a003d05d1a64965f0a6ecf63f40
Dan Engelbrecht <dan@engelbrecht.com>
2020-06-04 18:09:12 +0200

    fixed BASE_DIR inconsistencies

--- test/build_options.bat
+++ test/build_options.bat
@@ -3,4 +3,4 @@ set TARGET_TYPE=EXECUTABLE
 
 call ..\default_build_options.bat
 
-set MAIN_SRC=%BASE_DIR%\test\main.cpp %BASE_DIR%\test\test.cpp
+set MAIN_SRC=%BASE_DIR%test\main.cpp %BASE_DIR%test\test.cpp
