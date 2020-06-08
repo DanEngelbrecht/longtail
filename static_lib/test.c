3085428293df23529b3f6579e8b545029a6d0b38
Dan Engelbrecht <dan@engelbrecht.com>
2020-06-09 00:35:29 +0200

    clean up test.c

--- static_lib/test.c
+++ static_lib/test.c
@@ -3,14 +3,12 @@
 #include "../src/longtail.h"
 #include "../lib/filestorage/longtail_filestorage.h"
 
-// clang test.c -o ../build/shared_lib_test -ldl -fsanitize=address -fno-omit-frame-pointer
-
 int main()
 {
-//    void* p = Longtail_Alloc(16);
+    void* p = Longtail_Alloc(16);
     struct Longtail_StorageAPI* fs = Longtail_CreateFSStorageAPI();
     Longtail_DisposeAPI(&fs->m_API);
-//	Longtail_Free(p);
+	Longtail_Free(p);
     printf("Pass\n");
     return 0;
 }
