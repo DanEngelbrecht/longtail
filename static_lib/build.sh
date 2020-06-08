6e202b7360f2255144c83d70806f14bcd5be0526
Dan Engelbrecht <dan@engelbrecht.com>
2020-06-09 00:17:30 +0200

    spelling error

--- static_lib/build.sh
+++ static_lib/build.sh
@@ -90,9 +90,9 @@ pushd ${BASE_DIR}build
 ${COMPILER} -c ${CXXFLAGS} ${BASE_DIR}/static_lib/test.c
 popd
 
-cp ${LIB_TARGET} $lib{LIB_FILENAME}.a
+cp ${LIB_TARGET} lib${LIB_FILENAME}.a
 
-${COMPILER} -o ${TEST_EXECUTABLEPATH} ${CXXFLAGS} ${BASE_DIR}build/test.o -lm -L. -l:./$lib{LIB_FILENAME}.a --verbose
+${COMPILER} -o ${TEST_EXECUTABLEPATH} ${CXXFLAGS} ${BASE_DIR}build/test.o -lm -L. -l:./lib${LIB_FILENAME}.a --verbose
 
 
 #-l:${LIB_FILENAME}.a -L${BASE_DIR}build -lm  --verbose
