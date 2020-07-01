8fb3f220455ec3febb35a27930e53dfadf83c8ac
Dan Engelbrecht <dan@engelbrecht.com>
2020-07-01 12:14:03 +0200

    build options for perf test on linux

--- perf/build_options.sh
+++ perf/build_options.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export TARGET=test
+export TARGET=perf
 export TARGET_TYPE=EXECUTABLE
 
 . ../all_sources.sh
 . ../default_build_options.sh
 
-export MAIN_SRC="$BASE_DIR/test/main.cpp $BASE_DIR/test/test.cpp"
+export MAIN_SRC="$BASE_DIR/perf/main.cpp
