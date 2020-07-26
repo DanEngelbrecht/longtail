39d3c1f76b6149ebac36a1b7be109bb62aa5df73
Dan Engelbrecht <dan@engelbrecht.com>
2020-07-26 23:18:04 +0200

    include blockstorestorage and lrublockstore in dist scripts

--- dist.sh
+++ dist.sh
@@ -21,12 +21,14 @@ mkdir dist/include/lib/atomiccancel
 mkdir dist/include/lib/bikeshed
 mkdir dist/include/lib/blake2
 mkdir dist/include/lib/blake3
+mkdir dist/include/lib/blockstorestorage
 mkdir dist/include/lib/brotli
 mkdir dist/include/lib/cacheblockstore
 mkdir dist/include/lib/compressblockstore
 mkdir dist/include/lib/compressionregistry
 mkdir dist/include/lib/filestorage
 mkdir dist/include/lib/fsblockstore
+mkdir dist/include/lib/lrublockstore
 mkdir dist/include/lib/hashregistry
 mkdir dist/include/lib/lz4
 mkdir dist/include/lib/memstorage
@@ -39,12 +41,14 @@ cp lib/atomiccancel/*.h dist/include/lib/atomiccancel
 cp lib/bikeshed/*.h dist/include/lib/bikeshed
 cp lib/blake2/*.h dist/include/lib/blake2
 cp lib/blake3/*.h dist/include/lib/blake3
+cp lib/blockstorestorage/*.h dist/include/lib/blockstorestorage
 cp lib/brotli/*.h dist/include/lib/brotli
 cp lib/cacheblockstore/*.h dist/include/lib/cacheblockstore
 cp lib/compressblockstore/*.h dist/include/lib/compressblockstore
 cp lib/compressionregistry/*.h dist/include/lib/compressionregistry
 cp lib/filestorage/*.h dist/include/lib/filestorage
 cp lib/fsblockstore/*.h dist/include/lib/fsblockstore
+cp lib/lrublockstore/*.h dist/include/lib/lrublockstore
 cp lib/hashregistry/*.h dist/include/lib/hashregistry
 cp lib/lz4/*.h dist/include/lib/lz4
 cp lib/memstorage/*.h dist/include/lib/memstorage
