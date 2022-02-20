636c2ad8b9596e9e572954384bae3d28bc736cf4
Dan Engelbrecht <dan@engelbrecht.com>
2022-02-20 23:35:39 +0100

    Add support for VS2022 community edition

--- build_helper.bat
+++ build_helper.bat
@@ -100,6 +100,12 @@ if NOT DEFINED VCINSTALLDIR (
 )
 
 if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
     )
