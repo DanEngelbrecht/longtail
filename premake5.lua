95c9279e77c63dd5014a069a0caddd756a9e88fc
Dan Engelbrecht <dan@engelbrecht.com>
2022-05-17 22:38:08 +0200

    update premake and fix return param check on win32 (#201)

--- premake5.lua
+++ premake5.lua
@@ -12,30 +12,53 @@ workspace "longtail"
    location ( _OPTIONS["to"] )
    intrinsics "on"
    vectorextensions "AVX2"
-   runtime "Release"
+   symbols "On"
+   exceptionhandling ("off")
+   defines { '_CRT_SECURE_NO_DEPRECATE', '_CRT_SECURE_NO_WARNINGS', '_CRT_NONSTDC_NO_WARNINGS', '/D_HAS_EXCEPTIONS=0' }
 
-   configuration 'vs*'
-      defines     { '_CRT_SECURE_NO_DEPRECATE', '_CRT_SECURE_NO_WARNINGS', '_CRT_NONSTDC_NO_WARNINGS' }   
+   filter "configurations:Debug"
+      runtime "Debug"
+      defines { "DEBUG" }
+
+   filter "configurations:Release"
+      runtime "Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+project "longtail-src"
+   kind "StaticLib"
+   language ("C")
+   files { 'src/*.c', 'src/*.h', 'src/ext/*.h', 'src/ext/*.c'}
 
 project "longtail-lib"
    kind "StaticLib"
+   language ("C")
+   warnings "Default"
+   files { 'lib/**/*.c', 'lib/**/*.h', 'lib/*.c', 'lib/*.h' }
+   removefiles { 'lib/**/ext/**'}
 
-   files { 'lib/**.c', 'lib/**.h', 'src/**.c', 'src/**.h' }
+project "longtail-lib-ext"
+   kind "StaticLib"
+   language ("C")
+   warnings "Off"
+   files { 'lib/**/ext/**.c', 'lib/**/ext/**.h'}
+   removefiles { 'lib/blake3/ext/*_neon.c'}
 
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
-      
 project "longtail-cli"
-    kind "ConsoleApp"
-    files { 'cmd/**.c', 'cmd/**.h' }
-    links { "longtail-lib" }
+   kind "ConsoleApp"
+   language ("C")
+   warnings "Default"
+   files { 'cmd/*.c', 'cmd/*.h', 'cmd/ext/*.h' }
+   links { "longtail-src" }
+   links { "longtail-lib" }
+   links { "longtail-lib-ext" }
 
 project "longtail-tests"
-    kind "ConsoleApp"
-    files { 'test/**.cpp', 'test/**.h' }    
-    links { "longtail-lib" }
+   kind "ConsoleApp"
+   debugdir ("test")
+   language ("C++")
+   warnings "Default"
+   files { 'test/*.cpp', 'test/*.h', 'test/ext/*.h' }
+   links { "longtail-src" }
+   links { "longtail-lib" }
+   links { "longtail-lib-ext" }
