d75bec434455cc17a9504c74af1a869c7a8e031a
Dan Engelbrecht <dan@engelbrecht.com>
2024-01-04 11:11:25 +0100

    fix declarations

--- src/ext/stb_ds.c
+++ src/ext/stb_ds.c
@@ -4,8 +4,7 @@
 
 #ifdef STBDS_REALLOC
 
-extern void* Longtail_STBRealloc(void* context, void* old_ptr, size_t size);
-extern void Longtail_STBFree(void* context, void* ptr);
+#include "../longtail.h"
 
 #endif // STBDS_REALLOC
 
@@ -13,15 +12,12 @@ extern void Longtail_STBFree(void* context, void* ptr);
 
 #ifdef STBDS_REALLOC
 
-extern void* Longtail_ReAlloc(const char* context, void* old, size_t s);
-extern void Longtail_Free(void* p);
-
-inline void* Longtail_STBRealloc(void* context, void* old_ptr, size_t size)
+void* Longtail_STBRealloc(void* context, void* old_ptr, size_t size)
 {
 	return Longtail_ReAlloc("stb", old_ptr, size);
 }
 
-inline void Longtail_STBFree(void* context, void* ptr)
+void Longtail_STBFree(void* context, void* ptr)
 {
 	Longtail_Free(ptr);
 }
